6fc540da3ea7e48790fcf3cdfef4aeb89ce31550
Kelvinrosario <kelvinrosario377@gmail.com>
2024-05-25 18:13:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
+
+function clickLike1(){
+  let totalLikes = (countLike1. value) + 1
+  countLike1.textContent = totalLikes.toString()
+  
+  
+}
+
